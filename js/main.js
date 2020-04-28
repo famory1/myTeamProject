20a616a5cdd938197710a7e42a337b0f70d8070f
famory1 <63998288+famory1@users.noreply.github.com>
2020-04-28 16:41:20 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
 		$('.header__burger,.header__nav').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	$('.slider').slick({
+		adaptiveHeight:true,
+		dots:true,
+		autoplay: true,
+  		autoplaySpeed: 3000,
+  		cssEase: 'ease-out',
+  		speed: 1000,
+	});
 });

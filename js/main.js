ec91e5deb1bf9757adff465a96e90dc283182388
famory1 <63998288+famory1@users.noreply.github.com>
2020-04-30 16:29:08 +0300

    new block

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Слайдер и бургерт меню
 $(document).ready(function(){
 	$('.header__burger').click(function(event){
 		$('.header__burger,.header__nav,.header__logo').toggleClass('active');
@@ -12,3 +13,42 @@ $(document).ready(function(){
   		speed: 1000,
 	});
 });
+// Фильтр
+$(function(){
+
+	let filter = $("[data-filter]");
+
+	filter.on("click", function(event){
+		// При нажатии на наш фильтр, страница не будет обновляться и перекидывать нас в начало
+		event.preventDefault();
+		let cat = $(this).data('filter');
+		if(cat == 'all'){
+			$("[data-cat]").removeClass('hide');
+		}else{
+			$("[data-cat]").each(function(){
+			let workCat = $(this).data('cat');
+			console.log(workCat);
+			if (workCat != cat){
+				$(this).addClass('hide');
+			}else{
+				$(this).removeClass('hide');
+			}
+
+		});
+		}
+	});
+
+});
+//Плавная прокрутка до нужного раздела 
+// $(function(){
+
+// 	$('.portfolio').on('click', function(event){
+// 	  $('html,body').stop().animate({ scrollTop: $('.photo__body').offset().top }, 1000);
+// 	  event.preventDefault();
+// 	});
+// 	$('.home').on('click', function(event){
+// 	  $('html,body').stop().animate({ scrollTop: $('.slider').offset().top }, 1000);
+// 	  event.preventDefault();
+// 	});
+
+// });
